114d77570465eeafe6b763a0d3624bf9c2f1258b
hikaru <274173368@qq.com>
2022-03-20 17:46:51 +0800

    添加windicss

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import "vant/lib/index.css";
+import "virtual:windi.css";
 
 import Vant from "vant";
 import { Lazyload } from "vant";
