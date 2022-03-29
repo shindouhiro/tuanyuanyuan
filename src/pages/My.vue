b19565d24a5ef5c03e96cb095c073a5618fee578
Suoguozhen <suguozhen@meiti.com>
2022-03-29 23:03:59 +0800

    我的页面增加背景后,未成功怀疑被吃掉

--- src/pages/My.vue
+++ src/pages/My.vue
@@ -1,10 +1,15 @@
 <template>
-  <van-row justify="center">
-    <van-image
+  <van-row>sdfsdf sdf sdf 
+    <div class="bg-gray-900 h-4/5 w-4/6"></div>
+    
+      <van-image
+      position="right"
       round
-      width="10rem"
-      height="10rem"
+      width="5rem"
+      height="5rem"
+    
       src="https://img.yzcdn.cn/vant/cat.jpeg"
+      
     />
   </van-row>
   <van-cell-group>
@@ -20,3 +25,13 @@
     <van-cell title="路由跳转" is-link />
   </van-cell-group>
 </template>
+
+
+<style>
+:deep(.van-row) {
+  display: block;
+  background: #000;
+  width:300px;
+  height:500px;
+}
+</style>
