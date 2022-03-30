0867806a41c5a18f49825bcf7740bf6f646bcb80
hikaru <274173368@qq.com>
2022-03-30 23:44:21 +0800

    圆角

--- src/pages/My.vue
+++ src/pages/My.vue
@@ -1,43 +1,32 @@
 <template>
-  <van-row 
-  src="../../static/images/my_cetern.png">
-    <p class="text-4xl font-medium pt-12 pr-18 pl-10">手机号登录</p>
-   
-
-    <van-image
+  <van-row src="../../static/images/my_cetern.png">
+    <!-- <van-image
       display="block"
-      
       round
       width="5rem"
       height="5rem"
-    pr-0 
-     
-      
-      src="https://img.yzcdn.cn/vant/cat.jpeg" 
-    />
+      pr-0
+      src="https://img.yzcdn.cn/vant/cat.jpeg"
+    /> -->
+    <div class="nav"></div>
   </van-row>
   <van-cell-group>
-    <van-cell title="URL 跳转" is-link />
-    <van-cell title="路由跳转" is-link />
-    <van-cell title="URL 跳转" is-link />
-    <van-cell title="路由跳转" is-link />
-    <van-cell title="URL 跳转" is-link />
-    <van-cell title="路由跳转" is-link />
-    <van-cell title="URL 跳转" is-link />
-    <van-cell title="路由跳转" is-link />
-    <van-cell title="URL 跳转" is-link />
-    <van-cell title="路由跳转" is-link />
+    <van-cell title="我的收藏" is-link icon="location-o" />
+    <van-cell title="关于我们" is-link icon="location-o" />
   </van-cell-group>
 </template>
 
 <style scoped>
 .van-row {
-  
-  width:100%;
-  height:auto;
-  
-
+  width: 100%;
+  height: auto;
 }
-
-
+.nav {
+  width: 379px;
+  margin: 0 auto;
+  height: 53px;
+  background: #4759ee;
+  border-top-left-radius: 2em;
+  border-top-right-radius: 2em;
+}
 </style>
